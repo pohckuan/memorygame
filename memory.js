388c5685341dfb0dd4b50764656da1d147ca4f45
Poh Chuan Kuan <pohckuan@yahoo.com>
2017-05-17 14:46:06 -0400

    startTimer()

--- memory.js
+++ memory.js
@@ -1,4 +1,8 @@
-
+// global variables
+var clickedArray = [];
+var interval;
+var started = false;
+var time = 0;
 
 // execute functions here
 setUp();
@@ -13,6 +17,22 @@ function randomAnswers(){
   return answers;
 }
 
+function reveal(cell){
+  cell.style.backgroundColor = 'red';
+  cell.innerHTML = cell.value;
+  cell.clicked = true;
+}
+
+function startTimer(){
+  if(started == false){
+    interval = setInterval(function(){
+      time++;
+      document.getElementById('timer').innerHTML = "Time Elapsed: " + time;
+    }, 1000)
+    started = true;
+
+  }
+}
 function setUp(){
   var grid = document.getElementsByTagName('td');
   var answers = randomAnswers();
@@ -34,7 +54,10 @@ function setUp(){
   });
 
   cell.addEventListener("click", function(){
-
+    if(this.clicked == false && this.completed == false){
+      clickedArray.push(this);
+      reveal(this);
+    }
 
   })
   }
