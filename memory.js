a45a537c1a6bea3854fa9399b618f6a3745f50d7
Poh Chuan Kuan <pohckuan@yahoo.com>
2017-05-17 14:31:12 -0400

    added mouseover and mouseleave

---
+++ memory.js
@@ -0,0 +1,41 @@
+
+
+// execute functions here
+setUp();
+
+// function definitions go here:
+
+function randomAnswers(){
+  var answers = [1,1,2,2,3,3,4,4,5];
+  answers.sort(function(item){
+    return .5 - Math.random();
+  })
+  return answers;
+}
+
+function setUp(){
+  var grid = document.getElementsByTagName('td');
+  var answers = randomAnswers();
+
+  for(var i = 0; i < grid.length; i++){
+    var cell = grid[i];
+    cell.completed = false;
+    cell.clicked = false;
+    cell.value = answers[i];
+
+  cell.addEventListener("mouseenter", function(){
+    if(this.completed == false && this.clicked == false)
+      this.style.background = "orange";
+  });
+
+  cell.addEventListener("mouseleave", function(){
+    if(this.completed == false && this.clicked == false)
+      this.style.background = "pink";
+  });
+
+  cell.addEventListener("click", function(){
+
+
+  })
+  }
+}
